f47b2a16c65fc1a721b41115df8f327186aedd23
chy <308604256@qq.com>
2024-06-28 21:51:48 +0800

    trl_NCAinfra

---
+++ data_utils.py
@@ -0,0 +1,239 @@
+# Copyright 2022 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Union
+
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from transformers import PreTrainedModel, PreTrainedTokenizerBase
+
+@dataclass
+class NCADataCollatorWithPadding:
+    r"""
+    Contrast DataCollator class that pads the inputs to the maximum length of the batch.
+    Args:
+        tokenizer (`PreTrainedTokenizerBase`):
+            The tokenizer used for encoding the data.
+        model (Optional[`PreTrainedModel`]):
+            The model that is being trained. If set and has the *prepare_decoder_input_ids_from_labels*, use it to
+            prepare the *decoder_input_ids*.
+        padding (`Union[bool, str, `PaddingStrategy`]`, `optional`, defaults to `True`):
+            padding_strategy to pass to the tokenizer.
+        max_length (`Optional[int]`, `optional`, defaults to `None`):
+            The maximum length of the sequence to be processed.
+        max_prompt_length (`Optional[int]`, `optional`, defaults to `None`):
+            The maximum length of the prompt to be processed.
+        label_pad_token_id (`int`, defaults to -100):
+            The label used for masking.
+        padding_value (`int`, defaults to 0):
+            The value used for padding.
+        is_encoder_decoder (`Optional[bool]`, `optional`, defaults to `None`):
+            Whether or not you model has an encoder_decoder architecture.
+        max_target_length (`Optional[int]`, `optional`, defaults to `None`):
+            The maximum length of the target to be processed. Only useful for encoder-decoder architectures.
+        truncation_mode: (`str`, defaults to "keep_end"):
+            The truncation mode to use when truncating the prompt.
+    """
+    tokenizer: PreTrainedTokenizerBase
+    model: Optional[PreTrainedModel] = None
+    padding: Union[bool, str] = True
+    max_length: Optional[int] = None
+    max_prompt_length: Optional[int] = None
+    label_pad_token_id: int = -100
+    padding_value: int = 0
+    truncation_mode: str = "keep_end"
+    is_encoder_decoder: Optional[bool] = False
+    max_target_length: Optional[int] = None
+    optimize_prompt: Optional[bool] = False
+
+    def tokenize_batch_element(
+        self,
+        item,
+    ) -> Dict:
+        """Tokenize a single batch element.
+
+        At this stage, we don't convert to PyTorch tensors yet; we just handle the truncation
+            in case the prompt + chosen or prompt + rejected responses is/are too long. First
+            we truncate the prompt; if we're still too long, we truncate the chosen/rejected.
+
+        We also create the labels for the chosen/rejected responses, which are of length equal to
+            the sum of the length of the prompt and the chosen/rejected response, with
+            label_pad_token_id  for the prompt tokens.
+        """
+        batch = {}
+
+        if not self.is_encoder_decoder:
+            A0_tokens = self.tokenizer(item["A0"], add_special_tokens=False)#{"input_ids": "attention_mask":}
+            A1_tokens = self.tokenizer(item["A1"], add_special_tokens=False)#{"input_ids": "attention_mask":}
+            A2_tokens = self.tokenizer(item["A2"], add_special_tokens=False)#{"input_ids": "attention_mask":}
+            A3_tokens = self.tokenizer(item["A3"], add_special_tokens=False)#{"input_ids": "attention_mask":}
+            prompt_tokens = self.tokenizer(item["prompt"], add_special_tokens=False)
+
+            eos_token_id = self.tokenizer.eos_token_id
+            # Get indices in list prompt_tokens["input_ids"] that equals the EOS token (often 0)
+            eos_indices_prompt = [i for i, x in enumerate(prompt_tokens["input_ids"]) if x == eos_token_id]
+            # attention mask these indices to eos_token_id
+            new_attention_mask = [
+                0 if i in eos_indices_prompt else p for i, p in enumerate(prompt_tokens["attention_mask"])
+            ]
+            prompt_tokens["attention_mask"] = new_attention_mask
+
+            # do the same for A 0-3
+            eos_indices_A0 = [i for i, x in enumerate(A0_tokens["input_ids"]) if x == eos_token_id]
+            new_attention_mask_c = [
+                0 if i in eos_indices_A0 else p for i, p in enumerate(A0_tokens["attention_mask"])
+            ]
+            A0_tokens["attention_mask"] = new_attention_mask_c
+            A0_tokens["input_ids"].append(self.tokenizer.eos_token_id) # prompt is not added with eos finish
+            A0_tokens["attention_mask"].append(1)
+
+            eos_indices_A1 = [i for i, x in enumerate(A1_tokens["input_ids"]) if x == eos_token_id]
+            new_attention_mask_c = [
+                0 if i in eos_indices_A1 else p for i, p in enumerate(A1_tokens["attention_mask"])
+            ]
+            A1_tokens["attention_mask"] = new_attention_mask_c
+            A1_tokens["input_ids"].append(self.tokenizer.eos_token_id)
+            A1_tokens["attention_mask"].append(1)
+            
+            eos_indices_A2 = [i for i, x in enumerate(A2_tokens["input_ids"]) if x == eos_token_id]
+            new_attention_mask_c = [
+                0 if i in eos_indices_A2 else p for i, p in enumerate(A2_tokens["attention_mask"])
+            ]
+            A2_tokens["attention_mask"] = new_attention_mask_c
+            A2_tokens["input_ids"].append(self.tokenizer.eos_token_id)
+            A2_tokens["attention_mask"].append(1)
+            
+            eos_indices_A3 = [i for i, x in enumerate(A3_tokens["input_ids"]) if x == eos_token_id]
+            new_attention_mask_c = [
+                0 if i in eos_indices_A3 else p for i, p in enumerate(A3_tokens["attention_mask"])
+            ]
+            A3_tokens["attention_mask"] = new_attention_mask_c
+            A3_tokens["input_ids"].append(self.tokenizer.eos_token_id)
+            A3_tokens["attention_mask"].append(1)
+
+            longer_response_length = max(len(A0_tokens["input_ids"]), len(A1_tokens["input_ids"]), len(A2_tokens["input_ids"]), len(A3_tokens["input_ids"]))
+
+            if len(prompt_tokens["input_ids"]) + longer_response_length > self.max_length:#
+                if self.truncation_mode == "keep_start":
+                    prompt_tokens = {k: v[: self.max_prompt_length] for k, v in prompt_tokens.items()}
+                elif self.truncation_mode == "keep_end":
+                    prompt_tokens = {k: v[-self.max_prompt_length :] for k, v in prompt_tokens.items()}
+                else:
+                    raise ValueError(f"Unknown truncation mode: {self.truncation_mode}")
+
+            # if that's still too long, truncate the response
+            if len(prompt_tokens["input_ids"]) + longer_response_length > self.max_length:
+                A0_tokens = {k: v[: self.max_length - self.max_prompt_length] for k, v in A0_tokens.items()}
+                A1_tokens = {k: v[: self.max_length - self.max_prompt_length] for k, v in A1_tokens.items()}
+                A2_tokens = {k: v[: self.max_length - self.max_prompt_length] for k, v in A2_tokens.items()}
+                A3_tokens = {k: v[: self.max_length - self.max_prompt_length] for k, v in A3_tokens.items()}
+
+            # Create labels
+            A0_sequence_tokens = {k: prompt_tokens[k] + A0_tokens[k] for k in A0_tokens}
+            A1_sequence_tokens = {k: prompt_tokens[k] + A1_tokens[k] for k in A1_tokens}
+            A2_sequence_tokens = {k: prompt_tokens[k] + A2_tokens[k] for k in A2_tokens}
+            A3_sequence_tokens = {k: prompt_tokens[k] + A3_tokens[k] for k in A3_tokens}        
+            
+            A0_sequence_tokens["labels"] = A0_sequence_tokens["input_ids"][:]
+            A1_sequence_tokens["labels"] = A1_sequence_tokens["input_ids"][:]
+            A2_sequence_tokens["labels"] = A2_sequence_tokens["input_ids"][:]
+            A3_sequence_tokens["labels"] = A3_sequence_tokens["input_ids"][:]
+ 
+            if not self.optimize_prompt:
+                A0_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [self.label_pad_token_id] * len(
+                        prompt_tokens["input_ids"]
+                    )
+                A1_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [self.label_pad_token_id] * len(
+                        prompt_tokens["input_ids"]
+                    )
+                A2_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [self.label_pad_token_id] * len(
+                        prompt_tokens["input_ids"]
+                    )
+                A3_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [self.label_pad_token_id] * len(
+                        prompt_tokens["input_ids"]
+                    )
+            for k, toks in {
+                "A0": A0_sequence_tokens,
+                "A1": A1_sequence_tokens,
+                "A2": A2_sequence_tokens,
+                "A3": A3_sequence_tokens,
+                "prompt": prompt_tokens,
+            }.items():
+                for type_key, tokens in toks.items():
+                    if type_key == "token_type_ids":
+                        continue
+                    batch[f"{k}_{type_key}"] = tokens
+
+        else:
+            raise NotImplementedError
+
+        batch["prompt"] = item["prompt"]
+        batch["A0"] = item["prompt"] + item["A0"]
+        batch["A0_response_only"] = item["A0"]
+        batch["A1"] = item["prompt"] + item["A1"]
+        batch["A1_response_only"] = item["A1"]
+        batch["A2"] = item["prompt"] + item["A2"]
+        batch["A2_response_only"] = item["A2"]
+        batch["A3"] = item["prompt"] + item["A3"]
+        batch["A3_response_only"] = item["A3"]
+        
+        batch["A0_score"] = item["score_A0"]
+        batch["A1_score"] = item["score_A1"]
+        batch["A2_score"] = item["score_A2"]
+        batch["A3_score"] = item["score_A3"]
+        
+        return batch
+
+    def collate(self, batch):
+        # first, pad everything to the same length
+        padded_batch = {}
+        for k in batch[0].keys():
+            if k.endswith("_input_ids") or k.endswith("_attention_mask") or k.endswith("_labels"):
+                if self.is_encoder_decoder:
+                    raise NotImplementedError
+                else:
+                    # adapted from https://stackoverflow.com/questions/73256206
+                    if "prompt" in k:
+                        to_pad = [torch.LongTensor(ex[k][::-1]) for ex in batch]
+                    else:
+                        to_pad = [torch.LongTensor(ex[k]) for ex in batch]
+                    if k.endswith("_input_ids"):
+                        padding_value = self.tokenizer.pad_token_id
+                    elif k.endswith("_labels"):
+                        padding_value = self.label_pad_token_id
+                    elif k.endswith("_attention_mask"):
+                        padding_value = self.padding_value
+                    else:
+                        raise ValueError(f"Unexpected key in batch '{k}'")
+
+                    padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
+                    # for the prompt, flip back so padding is on left side
+                    if "prompt" in k:
+                        padded_batch[k] = padded_batch[k].flip(dims=[1])
+            elif k.endswith("_score"):
+                padded_batch[k] = torch.FloatTensor([ex[k] for ex in batch])
+            else:
+                padded_batch[k] = [ex[k] for ex in batch]
+
+        return padded_batch
+
+    def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
+        tokenized_batch = []
+
+        for feature in features:
+            batch_element = self.tokenize_batch_element(feature)
+            tokenized_batch.append(batch_element)
+
+        # return collated batch
+        return self.collate(tokenized_batch)
---
+++ preference_trainer.py
@@ -0,0 +1,54 @@
+# Code modified from 
+import torch
+import torch.nn.functional as F
+from trl.trl import DPOTrainer
+from typing import Tuple
+from typing import Literal
+
+class NCATrainer_pairwise(DPOTrainer):
+    r"""
+    Implementation of the NCA algorithm in pairwise preference settings.
+    """
+
+    def __init__(*args, loss_type: Literal["InfoNCA", "NCA"] = "InfoNCA", **kwargs):
+        super().__init__(*args, loss_type=loss_type, **kwargs)
+
+    def dpo_loss(
+        self,
+        policy_chosen_logps: torch.FloatTensor,
+        policy_rejected_logps: torch.FloatTensor,
+        reference_chosen_logps: torch.FloatTensor,
+        reference_rejected_logps: torch.FloatTensor,
+        reference_free: bool = False,
+    ) -> Tuple[torch.FloatTensor, torch.FloatTensor, torch.FloatTensor]:
+        """Implementation of the InfoNCA/NCA loss in pairwise prefernece settings.
+
+        Args:
+            policy_chosen_logps: Log probabilities of the policy model for the chosen responses. Shape: (batch_size,)
+            policy_rejected_logps: Log probabilities of the policy model for the rejected responses. Shape: (batch_size,)
+            reference_chosen_logps: Log probabilities of the reference model for the chosen responses. Shape: (batch_size,)
+            reference_rejected_logps: Log probabilities of the reference model for the rejected responses. Shape: (batch_size,)
+            beta: Temperature parameter for the DPO loss, typically something in the range of 0.1 to 0.5. We ignore the reference model as beta -> 0.
+            reference_free: If True, we ignore the _provided_ reference model and implicitly use a reference model that assigns equal probability to all responses.
+
+        Returns:
+            A tuple of three tensors: (losses, chosen_rewards, rejected_rewards).
+            The losses tensor contains the DPO loss for each example in the batch.
+            The chosen_rewards and rejected_rewards tensors contain the rewards for the chosen and rejected responses, respectively.
+        """
+        chosen_rewards = self.beta * (policy_chosen_logps - reference_chosen_logps)
+        rejected_rewards = self.beta * (policy_rejected_logps - reference_rejected_logps)
+
+        if self.loss_type == "DPO" or self.loss_type == "InfoNCA":
+            # Pairwise InfoNCA is Equivalent to DPO loss.
+            losses = -F.logsigmoid(chosen_rewards - rejected_rewards)
+        elif self.loss_type == "NCA":
+            # Pairwise NCA differs from DPO by only one line of code.
+            losses = - F.logsigmoid(chosen_rewards) - 0.5 * F.logsigmoid(-chosen_rewards) - 0.5 * F.logsigmoid(-rejected_rewards)
+        elif self.loss_type == "biasedNCA":
+            # Might further prevent decreasing chosen logp
+            losses = - F.logsigmoid(chosen_rewards) - F.logsigmoid(-rejected_rewards)
+        else:
+            raise ValueError(f"Unknown loss type: {self.loss_type}. Should be one of ['DPO', 'InfoNCA', 'NCA', 'biasedNCA']")
+
+        return losses, chosen_rewards.detach(), rejected_rewards.detach()
